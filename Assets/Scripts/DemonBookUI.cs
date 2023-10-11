2a091c234d817ce683bdc8d25941d07c33c74dc3
AIE\s220779 <85329147+sguiedi@users.noreply.github.com>
2023-10-11 14:46:11 +1100

    The demon book now uses a list of images

--- Assets/Scripts/DemonBookUI.cs
+++ Assets/Scripts/DemonBookUI.cs
@@ -1,14 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DemonBookUI : MonoBehaviour
 {
+    public Image page;
+    public List<Sprite> pages;
+    int pageNumber = 0;
+
     private void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
             gameObject.SetActive(false);
+        }
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            pageNumber++;
+            if (pageNumber >= pages.Count) pageNumber = 0;
+            page.sprite = pages[pageNumber];
+        }
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            pageNumber--;
+            if (pageNumber < 0) pageNumber = pages.Count - 1;
+            page.sprite = pages[pageNumber];
         }
     }
 }
