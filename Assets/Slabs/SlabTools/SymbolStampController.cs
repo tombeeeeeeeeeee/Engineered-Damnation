4d562fcb279fad038f0dd5247919c2d6670b7b1d
Dex <85329147+sguiedi@users.noreply.github.com>
2023-11-16 16:43:31 +1100

    dial spinning interpolation

--- Assets/Slabs/SlabTools/SymbolStampController.cs
+++ Assets/Slabs/SlabTools/SymbolStampController.cs
@@ -1,5 +1,7 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UIElements;
 
 public class SymbolStampController : Focusable
 {
@@ -7,6 +9,7 @@ public class SymbolStampController : Focusable
     [SerializeField] Transform raycastPos;
     [SerializeField] MeshRenderer[] planes;
     [SerializeField] InteractionController playerPickUpScript;
+    [SerializeField] GameObject innerUI;
     int currentRing = 0; // 0=outer 1=inner
 
     //   cycle input : turn left and right
@@ -35,6 +38,17 @@ public class SymbolStampController : Focusable
     public override void UpDown(float userInput)
     {
         currentRing = (int)(userInput + 1) / (int) 2;
+
+        if (currentRing == 0)
+        {
+            ui.SetActive(false);
+            innerUI.SetActive(true);
+        }
+        else
+        {
+            ui.SetActive(true);
+            innerUI.SetActive(false);
+        }
     }
 
     public override void Action2(InputAction.CallbackContext context)
@@ -45,6 +59,7 @@ public class SymbolStampController : Focusable
 
     public override void Exit(InputAction.CallbackContext context)
     {
+        innerUI.SetActive(false);
         PressStamp();
         base.Exit(context);
     }
